b5f287ad57bb0fa548c0f725c1f4590ab75e77ce
stemangiola <mangiolastefano@gmail.com>
2020-02-05 09:32:00 +1100

    add filter method

--- R/methods.R
+++ R/methods.R
@@ -1425,3 +1425,87 @@ unite.ttSc <- function(data, col, ..., sep = "_", remove = TRUE, na.rm = FALSE)
 {
   unite_update_and_add_attr(data, col, ..., sep = sep, remove = remove, na.rm = na.rm)
 }
+
+#' Subset rows using column values
+#'
+#' `filter()` retains the rows where the conditions you provide a `TRUE`. Note
+#' that, unlike base subsetting with `[`, rows where the condition evaluates
+#' to `NA` are dropped.
+#'
+#' dplyr is not yet smart enough to optimise filtering optimisation
+#' on grouped datasets that don't need grouped calculations. For this reason,
+#' filtering is often considerably faster on [ungroup()]ed data.
+#'
+#' @section Useful filter functions:
+#'
+#' * [`==`], [`>`], [`>=`] etc
+#' * [`&`], [`|`], [`!`], [xor()]
+#' * [is.na()]
+#' * [between()], [near()]
+#'
+#' @section Grouped tibbles:
+#'
+#' Because filtering expressions are computed within groups, they may
+#' yield different results on grouped tibbles. This will be the case
+#' as soon as an aggregating, lagging, or ranking function is
+#' involved. Compare this ungrouped filtering:
+#'
+#'
+#' The former keeps rows with `mass` greater than the global average
+#' whereas the latter keeps rows with `mass` greater than the gender
+#'
+#' average.
+#' @family single table verbs
+#' @inheritParams arrange
+#' @param ... <[`tidy-eval`][dplyr_tidy_eval]> Logical predicates defined in
+#'   terms of the variables in `.data`.
+#'   Multiple conditions are combined with `&`. Only rows where the
+#'   condition evaluates to `TRUE` are kept.
+#' @param .preserve when `FALSE` (the default), the grouping structure
+#'   is recalculated based on the resulting data, otherwise it is kept as is.
+#' @return
+#' An object of the same type as `.data`.
+#'
+#' * Rows are a subset of the input, but appear in the same order.
+#' * Columns are not modified.
+#' * The number of groups may be reduced (if `.preserve` is not `TRUE`).
+#' * Data frame attributes are preserved.
+#' @section Methods:
+#' This function is a **generic**, which means that packages can provide
+#' implementations (methods) for other classes. See the documentation of
+#' individual methods for extra arguments and differences in behaviour.
+#'
+#' The following methods are currently available in loaded packages:
+#' @seealso [filter_all()], [filter_if()] and [filter_at()].
+#' @export
+#' @examples
+#'
+#' # Learn more in ?dplyr_tidy_eval
+############# START ADDED ttSc #####################################
+#' @export
+filter <- function (.data, ..., .preserve = FALSE)  {
+  UseMethod("filter")
+}
+
+#' @export
+filter.default <-  function (.data, ..., .preserve = FALSE)
+{
+  dplyr::filter(.data, ..., .preserve = .preserve)
+}
+
+#' @export
+filter.ttSc <- function (.data, ..., .preserve = FALSE)
+{
+
+  .data %>%
+    drop_class(c("ttSc", "tt")) %>%
+    dplyr::filter( ..., .preserve = .preserve) %>%
+
+    # Update seurat
+    update_object_sc() %>%
+
+    # Add class
+    add_class("tt") %>%
+    add_class("ttSc")
+
+}
